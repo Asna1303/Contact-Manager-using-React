0a9c8344ffe82aa126676e7a310fcb62d1cdd7df
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-05-09 21:07:32 +0530

    added the initial contacts

--- src/components/App.js
+++ src/components/App.js
@@ -6,8 +6,25 @@ import ContactList from './ContactList';
 
 
 function App() {
+  const contacts = [
+    {
+      id="1",
+      "name":"kuttan",
+      "email":"kuttan@gmail.com"
+    },
+    {
+      id="2",
+      "name":"anna",
+      "email":"anna@gmail.com"
+    },
+    {
+      id="3",
+      "name":"achu",
+      "email":"achu@gmail.com"
+    }
+  ]
   return (
-    <div>
+    <div classname="ui container">
       <Header/>
      <AddContact/>
       <ContactList/>

7057693317ed53d1d1547cd2da09642ea61cf47f
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-05-11 12:24:27 +0530

    added trash icon

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -7,7 +7,9 @@ const renderContactList = props.contacts.map((contact) => {
 <div classname="item">
     <div className="content">
         <div className="header">{contact.name}</div>
+        <div>{contact.email}</div>
     </div>
+    <i className="trash alternate outline icon"></i>
 </div>
 );
 });

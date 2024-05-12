16d2c2fab48d38fcb6a0695b467319803b20071b
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-05-12 20:35:49 +0530

    created a function called contactcard

---
+++ src/components/ContactCard.js
@@ -0,0 +1,10 @@
+import React from "react";
+const ContactCard = (props) => {
+
+<div className="item" key={contact.id}>
+            <div className="content">
+                <div className="header">{contact.name}</div>
+                <div>{contact.email}</div>
+            </div>
+            <i className="trash alternate outline icon"></i>
+        </div>
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,22 +1,20 @@
 import React from "react";
+
 const ContactList = (props) => {
-    console.logs(props);
-const renderContactList = props.contacts.map((contact) => {
+    console.log(props);
+    const renderContactList = props.contacts.map((contact) => (
+        <div className="item" key={contact.id}>
+            <div className="content">
+                <div className="header">{contact.name}</div>
+                <div>{contact.email}</div>
+            </div>
+            <i className="trash alternate outline icon"></i>
+        </div>
+    ));
 
-}return(
-<div classname="item">
-    <div className="content">
-        <div className="header">{contact.name}</div>
-        <div>{contact.email}</div>
-    </div>
-    <i className="trash alternate outline icon"></i>
-</div>
-);
-});
-return(
-
-    <div className="ui celled list">renderContactList</div>
-);
+    return (
+        <div className="ui celled list">{renderContactList}</div>
+    );
 }
 
 export default ContactList;

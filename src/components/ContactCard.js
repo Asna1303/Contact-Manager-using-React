d7a532978e105d54e90867b689493ea75e321ad6
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-05-12 20:38:07 +0530

    returned the jsx elements

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -1,10 +1,15 @@
 import React from "react";
 const ContactCard = (props) => {
+    return ({
+        <div className="item" key={contact.id}>
+                    <div className="content">
+                        <div className="header">{contact.name}</div>
+                        <div>{contact.email}</div>
+                    </div>
+                    <i className="trash alternate outline icon"></i>
+                </div>})
 
-<div className="item" key={contact.id}>
-            <div className="content">
-                <div className="header">{contact.name}</div>
-                <div>{contact.email}</div>
-            </div>
-            <i className="trash alternate outline icon"></i>
-        </div>
+
+}
+        
+export default ContactCard;

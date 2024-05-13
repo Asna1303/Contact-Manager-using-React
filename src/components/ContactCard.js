a729cd250f418cc853e2bba6cda97f9f9e02b5e6
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-05-13 19:30:17 +0530

    added online style

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -1,13 +1,13 @@
 import React from "react";
 const ContactCard = (props) => {
-    return ({
+    return (
         <div className="item" key={contact.id}>
                     <div className="content">
                         <div className="header">{contact.name}</div>
                         <div>{contact.email}</div>
                     </div>
-                    <i className="trash alternate outline icon"></i>
-                </div>})
+                    <i className="trash alternate outline icon" style={{color:"red", marginTop:"7px"}}></i>
+                </div>)
 
 
 }

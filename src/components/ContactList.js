b401544cd8ee7593d9f7fbe75df219b3d5cb07fc
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-05-14 21:53:53 +0530

    created the function rendercontactlist

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -4,16 +4,15 @@ import ContactCard from "./ContactCard";
 
 const ContactList = (props) => {
     console.log(props);
-    const renderContactList = props.contacts.map((contact) => (
-        <div className="item" key={contact.id}>
-            <div className="content">
-                <div className="header">{contact.name}</div>
-                <div>{contact.email}</div>
-            </div>
-            <i className="trash alternate outline icon"></i>
-        </div>
-    ));
-
+    const renderContactList = props.contacts.map((contact) => {
+        return (
+          <ContactCard
+            contact={contact}
+            clickHander={deleteConactHandler}
+            key={contact.id}
+          />
+        );
+      });
     return (
         <div className="ui celled list">{renderContactList}</div>
     );
